5ddcf8f7daacc0894c8783b0b548ee24b22d0d42
guoxiangCN <mzygdeaq@163.com>
2021-07-24 00:14:01 +0800

    auipc

--- auipc/auipc.s
+++ auipc/auipc.s
@@ -5,3 +5,18 @@
 # LA (Load Address) LA是伪指令
 # 编译器会根据实际情况利用auipc和其他指令自动生成正确的指令
 # LA RD, LABEL       #  la x5 ,foo
+
+# 利用auipc获取当前pc
+.section .text
+.global _start
+
+_start:
+	addi x0, x0, 0
+	mv t0, x0
+	auipc t0, 0		
+	nop
+stop:
+	nop
+	j stop
+
+.end
---
+++ branch/branch.s
@@ -0,0 +1,17 @@
+# BEQ      # BEQ  RS1, RS2, IMM
+# BNE      # BNE  RS1, RS2, IMM
+# BLT      # BLT  RS1, RS2, IMM
+# BLTU     # BLTU RS1, RS2, IMM
+# BGE      # BGE  RS1, RS2, IMM
+# BGEU     # BGEU RS1, RS2, IMM
+
+# 伪指令
+# 
+# 
+#
+#
+#
+
+# int i = 0; while(i < 5) i++;
+#
+#
